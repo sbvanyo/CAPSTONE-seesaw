7c8a661fe367d0f1144d1924ac5fc62b34839c69
Stacey Vanyo <sbvanyo@gmail.com>
2023-08-26 10:42:18 -0500

    visited array rendering to profile

--- components/Visited.js
+++ components/Visited.js
@@ -1,22 +1,22 @@
 // import { signOut } from '../utils/auth'; // TODO: COMMENT IN FOR AUTH
 import { useEffect, useState } from 'react';
-import { favoritePlaygrounds } from '../api/playgroundData';
+import { visitedPlaygrounds } from '../api/playgroundData';
 import { useAuth } from '../utils/context/authContext'; // TODO: COMMENT IN FOR AUTH
 import PlaygroundCard from './PlaygroundCard';
 
-function Favorites() {
+function Visited() {
   // Set a state for playgrounds
   const [playgrounds, setPlaygrounds] = useState([]);
   // Get user ID via useAuth hook
   const { user } = useAuth();
   // Make API call to get all the playgrounds
-  const getFavoritePlaygrounds = () => {
-    favoritePlaygrounds(user.uid).then(setPlaygrounds);
+  const getVisitedPlaygrounds = () => {
+    visitedPlaygrounds(user.uid).then(setPlaygrounds);
   };
 
   // Make API call to get all the playgrounds on component render
   useEffect(() => {
-    getFavoritePlaygrounds();
+    getVisitedPlaygrounds();
   }, []);
 
   return (
@@ -24,11 +24,11 @@ function Favorites() {
       <div className="d-flex flex-wrap">
         {/* Map over playgrounds here using PlaygroundCard component. Returns a PlaygroundCard component for every item mapped over */}
         {playgrounds.map((playground) => (
-          <PlaygroundCard key={playground.firebaseKey} playgroundObj={playground} onUpdate={getFavoritePlaygrounds} />
+          <PlaygroundCard key={playground.firebaseKey} playgroundObj={playground} onUpdate={getVisitedPlaygrounds} />
         ))}
       </div>
     </div>
   );
 }
 
-export default Favorites;
+export default Visited;
--- pages/profile.js
+++ pages/profile.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import User from '../components/User';
 import Favorites from '../components/Favorites';
+import Visited from '../components/Visited';
 
 export default function Profile() {
   return (
     <>
       <User />
+      <h3>visited playgrounds:</h3>
+      <Visited />
       <h3>i love these playgrounds the most:</h3>
       <Favorites />
     </>
