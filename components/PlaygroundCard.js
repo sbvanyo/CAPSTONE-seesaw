e1a434c52c3fa2c57ec10e4c20a1b91e48ae4502
Stacey Vanyo <sbvanyo@gmail.com>
2023-09-12 15:38:41 -0500

    refactored favorites/visited + heart/check buttons on cards

--- components/PlaygroundCard.js
+++ components/PlaygroundCard.js
@@ -1,23 +1,113 @@
 /* eslint-disable react/require-default-props */
-import React from 'react';
+import { React, useState, useEffect } from 'react';
 import { Card, Button } from 'react-bootstrap';
+import { HeartFill, Check } from 'react-bootstrap-icons';
 import Link from 'next/link';
 import PropTypes from 'prop-types';
 import { useAuth } from '../utils/context/authContext';
-// import { deletePlayground } from '../api/playgroundData';
+import {
+  addFavoritePlayground,
+  removeFavoritePlayground,
+  addVisitedPlayground,
+  removeVisitedPlayground,
+} from '../api/playgroundData';
+
 function PlaygroundCard({ playgroundObj }) {
   const { user } = useAuth();
+  // Set state to manage whether heart is filled in or not
+  const [isFavorite, setIsFavorite] = useState(false);
+  const [hasVisited, setHasVisited] = useState(false);
+
+  useEffect(() => {
+    if (playgroundObj.favoritedBy) {
+      setIsFavorite(playgroundObj.favoritedBy.includes(user.uid));
+    }
+  }, [playgroundObj.favoritedBy, user.uid]);
+
+  useEffect(() => {
+    if (playgroundObj.visitedBy) {
+      setHasVisited(playgroundObj.visitedBy.includes(user.uid));
+    }
+  }, [playgroundObj.visitedBy, user.uid]);
+
+  const toggleFavorite = () => {
+    if (isFavorite) {
+      removeFavoritePlayground(playgroundObj.firebaseKey, user.uid)
+        .then(() => {
+          setIsFavorite(false);
+        })
+        .catch((error) => {
+          console.error('Error removing favorite:', error);
+        });
+    } else {
+      addFavoritePlayground(playgroundObj.firebaseKey, user.uid)
+        .then(() => {
+          setIsFavorite(true);
+        })
+        .catch((error) => {
+          console.error('Error adding favorite:', error);
+        });
+    }
+  };
+
+  const toggleVisited = () => {
+    if (hasVisited) {
+      removeVisitedPlayground(playgroundObj.firebaseKey, user.uid)
+        .then(() => {
+          setHasVisited(false);
+        })
+        .catch((error) => {
+          console.error('Error removing visited:', error);
+        });
+    } else {
+      addVisitedPlayground(playgroundObj.firebaseKey, user.uid)
+        .then(() => {
+          setHasVisited(true);
+        })
+        .catch((error) => {
+          console.error('Error adding visited:', error);
+        });
+    }
+  };
 
   return (
     <Card className="playgroundCard">
-      <Card.Img variant="top" src={playgroundObj.image} alt={playgroundObj.name} className="playgroundImage" />
+      <Card.Img
+        variant="top"
+        src={playgroundObj.image}
+        alt={playgroundObj.name}
+        className="playgroundImage"
+      />
+
+      <HeartFill
+        className={isFavorite ? 'heart-filled' : 'heart-empty'}
+        onClick={toggleFavorite}
+        onKeyDown={(e) => {
+          if (e.key === ' ') {
+            toggleFavorite();
+          }
+        }}
+        aria-pressed={isFavorite ? 'true' : 'false'}
+      />
+
+      <Check
+        className={hasVisited ? 'check-filled' : 'check-empty'}
+        onClick={toggleVisited}
+        onKeyDown={(e) => {
+          if (e.key === ' ') {
+            toggleFavorite();
+          }
+        }}
+        aria-pressed={isFavorite ? 'true' : 'false'}
+      />
+
       <Card.Body>
         <Card.Title className="cardTitle">{playgroundObj.name}</Card.Title>
 
-        <p>
+        {/* <p>
           {playgroundObj.visitedBy.includes(user.uid) ? ' ✅ i\'ve been here! ' : ''}
           {playgroundObj.favoritedBy.includes(user.uid) ? '💛 i love this playground' : ''}
-        </p>
+        </p> */}
 
         {/* DYNAMIC LINK TO VIEW THE playground DETAILS  */}
         <Link href={`/playground/${playgroundObj.firebaseKey}`} passHref>
@@ -55,11 +145,6 @@ PlaygroundCard.propTypes = {
     water: PropTypes.bool,
     zip: PropTypes.string,
   }).isRequired,
-  // onUpdate: PropTypes.func,
 };
 
 export default PlaygroundCard;
-
-// PlaygroundCard.defaultProps = {
-//   onUpdate: PropTypes.func,
-// };
